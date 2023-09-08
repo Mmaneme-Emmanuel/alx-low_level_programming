5b849e0aabfa5a585facbbae89598fff0b897407
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-08 11:45:05 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,8 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-printf("Size of a char: %lu bytes(s)\n", sizeof(a));
+
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of an int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
