2db6fe0ac99150fd0dc9d6843849bfadde44abcb
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-09 04:05:27 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - working with numbers
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+
+return (0);
+
+}
