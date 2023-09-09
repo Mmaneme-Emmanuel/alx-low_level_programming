87091c67282bf6dbd9bf0b5cddba0a806544afcf
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-09 02:49:56 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+	*main - printing poth upper and lower cases
+	*Return: always 0 (success)
+	*/
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+for (i = 'A'; i <= 'Z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
