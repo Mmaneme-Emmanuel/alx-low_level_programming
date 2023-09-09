0d14463bc55736405345a591559e9cd372e39b63
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-09 02:16:50 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this will be a perfect time
+ *Return: Always  0 (Success)
+*/
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+putchar(i);
+	putchar('\n');
+	return (0);
+}
