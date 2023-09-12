a588be3ee81a5c524f1c838c4c1e665fc651c120
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-12 19:13:56 +0100

    0-putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,18 +12,16 @@ int main(void)
 int i;
 int j;
 
-for (i = 0; i < 9; i++)
+while (i = '0'; i < '9'; i++)
 {
-
-for (j = i + 1; j <= 9; j++)
+while (j = i + 1; j <= '9'; j++)
 {
-
 if (i != j)
 {
 putchar(i);
 putchar(j);
-if (i == '8' && j == '9')
-	continue;
+if (i == '0' && j == '1')
+continue;
 putchar(',');
 putchar(' ');
 }
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - three digits counting
+*Return: always 0 (success)
+*/
+int main(void)
+
+{
+int i;
+
+	for (i = '0'; i <= 9; i++)
+
+	putchar(i);
+
+int j;
+	for (j = '1'; j <= 9; j++);
+	putchar(j);
+int k;
+	for (k = '2'; k <= 9; k++);
+	putchar(k);
+
