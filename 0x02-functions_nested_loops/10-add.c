50acdb477e8a4f25877b2e0610ec557f54dc846d
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-13 23:44:41 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*add - entry point
+*Description: function that add two integers
+*and return the result
+*@n1: integer one
+*@n2: integer two
+*Return: return the sum of the both numbers
+*/
+int add(int n1, int n2)
+{
+int sum;
+sum = n1 + n2;
+return (sum);
+}
+
+
