c15985bc9b16622d57e5deba6c265064969054b0
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-18 19:10:03 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_rev - prints a string, in reverse, followed by a new line
+*@s: represents the string to be printed
+*Return: return string
+*/
+void print_rev(char *s)
+{
+int i;
+int j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	j++;
+
+for (i = j; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
