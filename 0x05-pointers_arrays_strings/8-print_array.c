27e095581c34c8c09dccce9ed9cd207d34862bbb
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-19 01:14:40 +0100

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_array - elements of an array of integers
+*@a:parameter 1
+*@n:parameter 2
+*/
+void print_array(int *a, int n)
+
+{
+int i;
+
+for (i = 0; i < 0; i++)
+{
+printf("%d", a[i]);
+{
+if (i < n - 1)
+
+printf(" ,");
+}
+printf("\n");
+}
+}
