fc5be1049bd147bc760505ad94d4e148ae2cb328
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-20 21:47:06 +0100

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,18 +3,22 @@
 *_strncpy - function that copies a string
 *@src: first issue to solve
 *@dest: second issue i will be solving
+*@n: to be sorted
 *Return: dest
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	char destl = 0;
-	int i = 0;
+int i = 0;
 
-for (i = 0; dest[i] != '\0'; i++)
-	destl++
-
-		for (i = 0; i <= n; i++)
-		dest[destl + i] = src[i];
-
+while (i < n && src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
+}
+while (i < n)
+{
+	dest[i] = '\0';
+i++;
+}
 return (dest);
-
+}
