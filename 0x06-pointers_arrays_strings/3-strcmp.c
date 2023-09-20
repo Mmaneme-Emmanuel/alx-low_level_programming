d48c1ce487777aed6c44641bfe5aa2f0647f1844
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-20 22:51:46 +0100

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,18 +3,19 @@
 *_strcmp - function that compares two strings
 *@s1: first string
 *@s2: second string
-*Return: return the highest value
+*Return: s1, s2
 */
 int _strcmp(char *s1, char *s2)
 {
-	int k = 0;
+int k = 0;
 
 while (s1[k] != '\0' && s2[k] != '\0')
-
-	if (s1[k] != s2[k])
-
-	return (s1[k] - s2[k]);
-	k++;
-
+{
+if (s1[k] != s2[k])
+{
+return (s1[k] - s2[k]);
+}
+k++;
+}
 return (0);
 }
