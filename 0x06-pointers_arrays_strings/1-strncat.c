e0ceec4a5487d028f09bd0e5ed92140376461077
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-20 20:21:51 +0100

    for (i = 0; dest[i] != 0; i++)

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,10 +9,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 char destl = 0;
+char srcl = 0;
 int i = 0;
 
 for (i = 0; dest[i] != '\0'; i++)
 destl++;
+
+for (i = 0; src[i] != '\0'; i++)
+	srcl++;
 
 for (i = 0; i <= n; i++)
 
