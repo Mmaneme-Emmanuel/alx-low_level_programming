c8a8c7297649e0203a6bab2d8efd8686b7053ba0
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-22 01:15:38 +0100

    101-main.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*print_number - main function
+*@n: parameter
+*Return: always 0
+*/
+
+void print_number(int n)
+
+{
+int n1;
+
+n1 = n;
+
+if (n < 0)
+
+{
+_putchar('-');
+
+n1 = -n;
+
+}
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
+}
