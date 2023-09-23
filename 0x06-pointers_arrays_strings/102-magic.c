75cc7ab236506261b11dca959873d612854684eb
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-23 13:03:29 +0100

    #include <stdio.h>

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include:"main .h
-
+#include <stdio.h>
 /**
 *function - that adds line to this code
 *@a: variable
 *Return: always return void
 */
-int maina (void)
+int main(void)
+
 {
 int n;
 int a[5];
@@ -21,11 +21,15 @@ p = &n;
 *_You are not allowed to use the variable a
 *_You are not allowed to modify the variable p
 *_only one statement
-*_You are not allowed to code anything else than the line of expected line of code
+*_You are not allowed to code anything elsethan
+*the line of expected line of code
 */
-p[5] = 98;
+
+*(p + 5) = 98;
+
 /*so that this prints 98\n*/
+
 printf("a[2] = %d\n", a[2]);
 
 return (0);
-
+}
