fd75fa071795ca15aa6dd762bfd43a577adea269
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-25 23:19:29 +0100

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
-*_chessboard - main function
+*print_chessboard - main function
 *@a:parameter *a
 *Return:Always 0.
 */
 void print_chessboard(char (*a)[8])
 {
-	int e;
-	int k;
+int e;
+int k;
 for (e = 0; e < 8; e++)
 {
-	for (k = 0; k < 8; k++)
-	}
+for (k = 0; k < 8; k++)
 _putchar(a[e][k]);
+
 _putchar('\n');
+}
 }
