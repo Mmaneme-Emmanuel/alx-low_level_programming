aa5fe03a28f21324c6bd5ed00112cd8788092962
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-28 20:47:30 +0100

    ./2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strlen_recursion - unction that returns the length of a string
+*@s: parameter s
+*Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+
+if (*s != '\0')
+{
+_putchar(1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
