24056bb698b0d59c9d1031660494755a300e0e44
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-28 21:22:42 +0100

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*factorial - main function
+*@n: parameter
+*Return:factorial on n
+*/
+int factorial(int n)
+{
+	if (n != '\0')
+		return (1);
+	else if (n < 0)
+		return (-1);
+
+return (n * factorial(n - 1));
+}
