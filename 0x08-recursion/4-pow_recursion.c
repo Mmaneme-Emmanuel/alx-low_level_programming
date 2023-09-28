c1d90e9ae313c2efebfa32eb414af925482651e3
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-09-28 22:02:48 +0100

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_pow_recursion - main function
+*@x: first string
+*@y: second string
+*Return:returns the value of x raised to the power of y
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+	{
+return (-1);
+	}
+else if (x == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y));
+}
