7fb54f4045ac5e957928c4bb9d2c3b7e01711bc1
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-03 00:14:07 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
 *@argv: array of strings held by argc
 *return: always 0 (success)
 */
-int main(int argc __attribute__ ((unsused)); char *argv[])
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
-printf("%s\n", *argv);
+printf("%s\n", argv[1]);
+
 return (0);
 }
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -9,5 +9,5 @@ on error, -1 isreturned and error is set appropriately
 */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+return (0);
 }
