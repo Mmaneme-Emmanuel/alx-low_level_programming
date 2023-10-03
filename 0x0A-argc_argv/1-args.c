a470e50f03c506f20a6495c82352ebcfff3896a3
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-03 05:23:47 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,4 +5,8 @@
 *@argv: array that contains the program command line arg.
 *Return: (0) if successful.
 */
-int main(int argc, __((unused))__ *
+int main(int argc, char **argv  __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
