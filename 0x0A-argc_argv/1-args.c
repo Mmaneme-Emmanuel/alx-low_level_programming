71037909f75cfffbcce7fa0b98749d8fb0656c84
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-03 04:37:57 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,6 @@
 *main - prints the number of arguments passed
 *@argc: number of command line argument.
 *@argv: array that contains the program command line arg.
-*Return: (0)
+*Return: (0) if successful.
 */
-int main(int argc, char **argv __attribute__((unused))
-{
-printf("%d\n", argc - 1);
-return (0);
-}
+int main(int argc, __((unused))__ *
