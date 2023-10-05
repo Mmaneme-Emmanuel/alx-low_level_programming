b45c9b02d4f925d95c90c43db2555678137eb06a
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-05 14:17:45 +0100

    Ek

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - function that returns a pointer to a newly
+ * allocated space in memory
+ *@str: my pointer
+ *Return: always NULL in every situation
+ */
+char *_strdup(char *str)
+{
+	char *Ek;
+	int i;
+	int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+while (str[i] != '\0')
+	i++;
+Ek = malloc(sizeof(char) * (i + 1));
+	if (Ek == NULL)
+		return (NULL);
+
+for (j = 0; str[j]; j++)
+Ek[j] = str[j];
+return (Ek);
+}
