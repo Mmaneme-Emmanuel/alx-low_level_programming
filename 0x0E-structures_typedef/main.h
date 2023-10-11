aee7f5e43d73ab3454490e41aa4086afb7fc30dc
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-11 20:35:26 +0100

    ./c

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include "main.h"
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
