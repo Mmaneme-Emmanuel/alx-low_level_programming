c81627c0cbd2cf420e2b3f467b0ddcd749a1ae38
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-11 18:19:59 +0100

    gob

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ *struct dog - it changes the dog info
+ *@name: name to initialize
+ *@age: age to initialize
+ *@owner: owner to initialize
+ *description: my first struct assignment
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
