a502fbf49722593a174fb570a41a0b21ad2922fc
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-11 21:09:08 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include "dog.h"
 /**
  *struct dog - it changes the dog info
  *@name: name to initialize
@@ -8,12 +9,18 @@
  *description: my first struct assignment
  */
 
+/**
+ *@dog_t: the new name for struct dog
+ */
+
+typedef struct dog dog_t;
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
- void init_dog(struct dog *d, char *name, float age, char *owner);
- void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
