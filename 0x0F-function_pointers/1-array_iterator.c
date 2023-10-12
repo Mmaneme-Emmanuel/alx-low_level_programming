e248adceedaf6b2d1e1bde4c3bb1c699d520d390
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-12 13:28:53 +0100

    ./b

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ *@size: size of the array
+ *@action: pointer to the function needed
+ *@array: atrgeted array
+ *Return: always void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+	
+	action(*array);
+	array++;
+	}	
+}
