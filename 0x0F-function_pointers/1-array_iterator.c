8eed08be853a1812c17b93626fc445079b2f54da
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-12 13:34:48 +0100

    _iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 /**
- *@size: size of the array
- *@action: pointer to the function needed
- *@array: atrgeted array
- *Return: always void
+ *array_iterator - the main function name
+ *@size:size of the array
+ *@action:pointer to the function needed
+ *@array:atrgeted array
+ *Return:always void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 
@@ -15,8 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	for (i = 0; i < size; i++)
 	{
-	
 	action(*array);
 	array++;
-	}	
+	}
 }
