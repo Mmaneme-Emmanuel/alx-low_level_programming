ccec573db7ad6472471d8f0bb507daab761b2a56
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-20 01:06:46 +0100

    function that prints all the elements of a list_t list2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,27 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 /**
- *print_list - function that prints all the elements
+ * print_list -  function that prints all the elements
  * @h:pointer to the const list
- * Return:always  list_t
+ * Return: total as the computed value
  */
 size_t print_list(const list_t *h)
 {
-	int total = 0;
+int total;
 
 
 if (h == NULL)
+	return (0);
+for (total = 1; h->next != NULL; total++)
+{
+if (h->str == NULL)
+printf("[%u] %s\n", h->len, "(nill)");
 
-printf("[0] (nill)\n");
+	else
 
-else
-
-printf("[%d] %s\n", h->len, h->str);
-
-total++;
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
+}
+printf("[%u] %s\n", h->len, h->str);
 
 return (total);
 }
