523f1148b67472682367a02c33f8190d8353d0d9
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-21 02:31:08 +0100

    function that prints

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * ekene - this is the first I ma using printf
+ */
+void ekene(void)
+{
+
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
