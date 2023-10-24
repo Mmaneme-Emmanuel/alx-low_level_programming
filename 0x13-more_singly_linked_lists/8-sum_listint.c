7cc3477b15042b0f40d439a4443650c824e92698
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-24 17:56:52 +0100

    function that returns the sum of all the data (n)

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *sum_listint - function that returns the sum of all the data (n)
+ *@head: the first node with the first address
+ *Return:sum of sum of all the data (n) of a listint_t
+ */
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+	if (head == NULL)
+		return (0);
+
+while (head != NULL)
+	{
+	add = head->n;
+		head = head->next;
+	}
+		return (add);
+}
