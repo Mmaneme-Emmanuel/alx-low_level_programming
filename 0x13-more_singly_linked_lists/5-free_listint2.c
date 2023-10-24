b369e9976dafccaeee1cab6cba8320c048413ccb
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-24 14:08:40 +0100

    4-free_listint.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,16 @@
 *@head: the head node of the listint
 *Return: returned a freed listint
 */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 
 listint_t *temp;
 
-while ((temp = head) != NULL)
-
+while (head != NULL)
+        temp = head;
+{
 head = head->next;
 free(temp);
+}
+head = NULL;
 }
