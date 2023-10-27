e2feb95fd82d68c0391f6b840115c652cc6fe0e0
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-27 07:42:29 +0100

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,22 +19,23 @@ size_t power = 1;
 int base = 2;
 
 
-	if (b == NULL)
-		return (0);
+if (b == NULL)
+return (0);
+{
 for (len = 0; b[len] != '\0'; len++)
 ;
 if (len == 1 && (b[0] == '0' || b[0] == '1'))
 return (b[0] - 48);
-
+}
 for (i = 0; b[i] != '\0'; i++)
 {
-	if (b[i] != '0' && b[i] != '1')
-	return (0);
-	for (j = len - 1; j > 0; j--)
-	power = power * base;
-	sum = sum + (power * (b[i] - 48));
-	len--;
-	power = 1;
-	}
-	return (sum);
+if (b[i] != '0' && b[i] != '1')
+return (0);
+for (j = len - 1; j > 0; j--)
+power = power *base;
+sum = sum + (power * (b[i] - 48));
+len--;
+power = 1;
 }
+return (sum);
+	}
