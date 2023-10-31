716c1da6828e5d2ad60bde706e33d363914798c0
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-31 16:48:30 +0100

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
 *error_file - checking for erros in a file
-*@argv: argument vector
 *@file_from: the name of the file to be checked
 *@file_to: destination that file will copy to
+*@argv: argument vector
 *Return: nothing
 */
 void error_file(int file_from, int file_to, char *argv[])
@@ -34,16 +36,13 @@ int file_from, file_to, err_close;
 ssize_t nchar, nwr;
 char buff[1024];
 
-
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 exit(97);
 }
-
 file_from = open(argv[1], O_RDONLY);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-
 error_file(file_from, file_to, argv);
 
 nchar = 1024;
@@ -51,25 +50,22 @@ while (nchar == 1024)
 {
 nchar = read(file_from, buff, 1024);
 if (nchar == -1)
-error_file(-1, 0, argv);
-
+	error_file(-1, 0, argv);
 nwr = write(file_to, buff, nchar);
-if (nwr == -1)
-
-error_file(-1, 0, argv);
-
+	if (nwr == -1)
+	error_file(0, -1, argv);
 }
 err_close = close(file_from);
 if (err_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
-exit(100);
+	dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+	exit(100);
 }
-err_close = close(file_from);
+err_close = close(file_to);
 if (err_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
-exit(100);
+	dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
+	exit(100);
 }
 return (0);
 }
