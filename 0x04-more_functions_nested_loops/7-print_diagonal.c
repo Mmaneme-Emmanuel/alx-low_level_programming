aa728a16459c3c5fa50faceaff89066aa8d2b5e3
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-16 22:29:10 +0100

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,19 +9,29 @@ void print_diagonal(int n)
 
 
 {
-	int i;
-
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (i = 0; i < n; i++)
+	else
 	{
-		_putchar(' ');
+		int i, j;
+
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+			_putchar(' ');
+				}
+			}
+				_putchar('\n');
+		}
 	}
-	_putchar('\\');
-	_putchar('\n');
 }
