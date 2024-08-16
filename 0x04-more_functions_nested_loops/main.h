14afdd1997878393efc638a0c8765df93fb5944b
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-17 00:06:18 +0100

    task 9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+int main(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
