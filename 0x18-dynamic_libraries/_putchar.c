5be68abe967f9dc7af69151a0133ea67a4c2a38b
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-16 16:36:29 +0100

    task 0 done

--- 0x18-dynamic_libraries/_putchar.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-/* _putchar writes the character c to the stdout*/
-
-int _putchar(char c)
-{
-	return(write(1, $c, 1));
-}
---
+++ 0x18-dynamic_libraries/functions/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checker for lower character
+ * @c: the character to be checked
+ * Return: i if c is lower, 0 otherwise.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
