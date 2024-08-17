40de6b09a4bb924ca9f5f77db4003b16770dcee5
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-17 13:13:46 +0100

    task 101 done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar.
+ * @n: the integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+	int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	while (num / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{_putchar((num / divisor) % 10 + '0');
+		divisor /= 10;
+	}
+}
