4c462446a8c4c9ce63c6b54a03b8fa1e7fece8a7
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-17 01:06:02 +0100

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,15 +8,15 @@
  */
 void print_triangle(int size)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
 
-        if (size <= 0)
-        {
-                _putchar('\n');
-                return;
-        }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
 
 	for (i = 1; i <= size; i++)
